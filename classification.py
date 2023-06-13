a85bc82312d9760449f8657188a7149166639584
safarisu <131063962+safarisu@users.noreply.github.com>
2023-06-13 10:17:37 +0200

    Add files via upload

--- classification.py
+++ classification.py
@@ -1,10 +1,12 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
 
 # Load the spam email dataset
 dataset = pd.read_csv('spam_assassin.csv')
@@ -27,6 +29,27 @@ classifier.fit(X_train_counts, y_train)
 y_pred = classifier.predict(X_test_counts)
 
 # Print the confusion matrix and classification report
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt="d")
+plt.show()
+
+clf = RandomForestClassifier(random_state = 42, n_jobs=-1)
+clf.fit(X_train_counts, y_train)
+y_pred = clf.predict(X_test_counts)
+
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt="d")
+plt.show()
+
+k=3
+KNN = KNeighborsClassifier(n_neighbors=k, n_jobs=-1)
+KNN.fit(X_train_counts, y_train)
+y_pred = KNN.predict(X_test_counts)
+
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 
